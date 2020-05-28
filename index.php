78f35d79ad02541933331d2b77ee753ec15a48dd
sdreher7991 <61548953+sdreher7991@users.noreply.github.com>
2020-05-28 13:58:45 +0200

    Update index.php
    Security checks added

--- index.php
+++ index.php
@@ -30,40 +30,47 @@
 
 <body>
 <?php
-    $php_output = "";
-    if (!isset($_POST['format_output']) || $_POST['format_output'])
-        $checkbox__format_output = 'checked="checked"';
-    else
-        $checkbox__format_output = '';
+    $WHITELIST_IP = '127.0.0.1'; 
+    function ip_adr() { 
+	if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) { 
+		$ip_adr = $_SERVER['HTTP_X_FORWARDED_FOR']; 
+	} else { 
+		$ip_adr = $_SERVER["REMOTE_ADDR"]; } 
+	return $ip_adr;
+	}
+    $client_ip = ip_adr();
+    echo '<h2>Your IP is '.$client_ip.'</h2>';
+    if ($client_ip == $WHITELIST_IP) {
 
-    if (@$_POST['php_input'])
-        {
-        if (get_magic_quotes_gpc()) 
-           $_POST['php_input'] = stripslashes($_POST['php_input']); 
-        ob_start();
-        eval($_POST['php_input']);
-        $php_output = ob_get_contents();
-        ob_end_clean();
+	    $php_output = "";
+	    if (!isset($_POST['format_output']) || $_POST['format_output'])
+		$checkbox__format_output = 'checked="checked"';
+	    else
+		$checkbox__format_output = '';
 
-        if (@$_POST['format_output'])
-            {
-            $php_output = preg_replace('/(?:^|\G) /m', '&nbsp;', $php_output);  // Replace leading spaces with &nbsp;
-            $php_output = str_replace ("\n", "<br />", $php_output);
-            }
-        }
-    else
-        {
-        $_POST['php_input'] = "echo '<h2>Your \$_SERVER array values:</h2>';\nprint_r (\$_SERVER);";
-        }
+	    if (@$_POST['php_input'])
+		{
+		if (get_magic_quotes_gpc()) 
+		   $_POST['php_input'] = stripslashes($_POST['php_input']); 
+		
+		ob_start();
+		eval($_POST['php_input']);
+		$php_output = ob_get_contents();
+		ob_end_clean();
 
+		if (@$_POST['format_output'])
+		    {
+		    $php_output = preg_replace('/(?:^|\G) /m', '&nbsp;', $php_output);  // Replace leading spaces with &nbsp;
+		    $php_output = str_replace ("\n", "<br />", $php_output);
+		    }
+		}
+	    else
+		{
+		$_POST['php_input'] = "echo '<h2>Your \$_SERVER array values:</h2>';\nprint_r (\$_SERVER);";
+		}
+	 ?>
 
-?>
 <div align="center" style="margin-top:30px;">
-<form action="http://php.net/search.php" method="get" target="_blank">
-<input name="show" type="hidden" value="quickref" />
-  <label for="pattern">Search PHP.NET for [function]:&nbsp;&nbsp;</label>
-  <input name="pattern" type="text" id="pattern" size="30" />&nbsp; <input name="submit" type="submit" value="Search PHP.NET ..." />
-</form>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <div style="margin:5px 0;">Type PHP code to execute:</div>
         <table width="100%" border="0" cellspacing="1" cellpadding="1" id="console-table">
@@ -99,5 +106,9 @@
 
     </form>
 </div>
+ <?php }  else {
+	   	echo '<h2>This site is only available from '.$WHITELIST_IP.'!</h2>';
+	   } ?>
+<p><a href="https://github.com/sdreher7991/PHP-console">PHP Web Console</a></p>
 </body>
 </html>
